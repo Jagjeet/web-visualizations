193dafe2c1761a507668fe8cf2ccbc1554614cdc
Jagjeet Khalsa <Jagjeet.Khalsa@gmail.com>
2021-10-05 22:14:12 -0700

    Add bar chart update and bubble chart init/update

--- static/js/app.js
+++ static/js/app.js
@@ -8,6 +8,7 @@ d3.json(samples).then(function(data) {
   // https://stackoverflow.com/questions/43121679/how-to-append-option-into-select-combo-box-in-d3
   let selector = d3.select("#selDataset");
 
+  // Event listeners
   selector.on("change", function(){
     let dropdownMenu = d3.select("#selDataset");
     // Assign the value of the dropdown menu option to a variable
@@ -20,6 +21,10 @@ d3.json(samples).then(function(data) {
     let sampleSamples = data.samples.filter(e => e.id === sampleName);
     console.log("Sample Samples: ");
     console.log(sampleSamples);
+
+    updateBarChart(sampleSamples[0]);
+    updateBubbleChart(sampleSamples[0]);
+
   });
 
   data.names.forEach((name) => {
@@ -34,19 +39,99 @@ d3.json(samples).then(function(data) {
       x: data.samples[0].sample_values.slice(0, 10).reverse(),
       y: data.samples[0].otu_ids.map(e => `OTU ${e}`).slice(0, 10).reverse(),
       text: data.samples[0].otu_labels.slice(0, 10).reverse(),
-
       type: "bar",
       orientation: 'h'
     }];
 
     let layout = {
       height: 600,
-      width: 800
+      width: 600
     };
 
     Plotly.newPlot("bar", barData, layout);
   }
-  initBarPlot();
 
+  // Function called by DOM changes
+  function updateBarChart(sample) {
+    let newBarData = {
+      x: [sample.sample_values.slice(0, 10).reverse()],
+      y: [sample.otu_ids.map(e => `OTU ${e}`).slice(0, 10).reverse()],
+      text: [sample.otu_labels.slice(0, 10).reverse()],
+      type: "bar",
+      orientation: 'h'
+    };
+
+    Plotly.restyle("bar", newBarData);
+  }
+
+  function initBubblePlot() {
+    // Create a bubble chart that displays each sample.
+    // Use otu_ids for the x values.
+    // Use sample_values for the y values.
+    // Use sample_values for the marker size.
+    // Use otu_ids for the marker colors.
+    // Use otu_labels for the text values.
+
+    let trace1 = {
+      x: data.samples[0].otu_ids,
+      y: data.samples[0].sample_values,
+      mode: 'markers',
+      marker: {
+        size: data.samples[0].sample_values,
+        color: data.samples[0].otu_ids,
+      },
+      text: data.samples[0].otu_labels
+    };
+
+    let bubbleData = [trace1];
+
+    let layout = {
+      title: `OTU IDs Vs Sample Values`,
+      showlegend: false,
+      height: 600,
+      width: 1200,
+      xaxis: {
+        title: {
+          text: 'OTU IDs',
+          font: {
+            family: 'Courier New, monospace',
+            size: 18,
+            color: '#7f7f7f'
+          }
+        }
+      },
+      yaxis: {
+        title: {
+          text: 'Sample Values',
+          font: {
+            family: 'Courier New, monospace',
+            size: 18,
+            color: '#7f7f7f'
+          }
+        }
+      }
+    };
+
+    Plotly.newPlot("bubble", bubbleData, layout);
+  }
+
+  // Function called by DOM changes
+  function updateBubbleChart(sample) {
+    let newBubbleData = {
+      x: [sample.otu_ids],
+      y: [sample.sample_values],
+      mode: 'markers',
+      marker: [{
+        size: sample.sample_values,
+        color: sample.otu_ids,
+      }],
+      text: [sample.otu_labels]
+    };
+
+    Plotly.restyle("bubble", newBubbleData);
+  }
+
+  initBarPlot();
+  initBubblePlot();
 
 });
